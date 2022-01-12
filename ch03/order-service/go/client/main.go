fe1f1622c368d3c1207f7190ad65a83920046a20
zhanglijie <lijie.z@funlink-tech.com>
2022-01-12 19:28:51 +0800

    dev

--- ch03/order-service/go/client/main.go
+++ ch03/order-service/go/client/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	wrapper "github.com/golang/protobuf/ptypes/wrappers"
 	"google.golang.org/grpc"
+	"io"
 	"log"
+	"time"
 
 	pb "ordermgt/client/ecommerce"
 )
@@ -18,5 +22,24 @@ func main() {
 	}
 	defer conn.Close()
 	client := pb.NewOrderManagementClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	//Add order
+	order1 := pb.Order{Id: "101", Items: []string{"iPhone XS", "Mac Book Pro"}, Destination: "San Jose, CA", Price: 2300.00}
+	res, _ := client.AddOrder(ctx, &order1)
+	if res != nil {
+		log.Print("AddOrder Response -> ", res.Value)
+	}
 
+	//Get Order
+	retrievedOrder, err := client.GetOrder(ctx, &wrapper.StringValue{Value: "106"})
+	log.Print("GetOrder Response -> ", retrievedOrder)
+	// Search Order : Server streaming scenario
+	searchStream, _ := client.SearchOrders(ctx, &wrapper.StringValue{Value: "Google"})
+	for {
+		searchOrder, err := searchStream.Recv()
+		if err == io.EOF {
+			log.Print("EOF")
+		}
+	}
 }
